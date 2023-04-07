ed78612cb07240f073d704f16976be0fbcf79fdf
ElvisBlue <elvis200070@gmail.com>
2023-04-07 09:45:33 +0700

    Delete CBreakpoints

--- x64dbgpython/x64dbgpython/BreakpointDlg.h
+++ x64dbgpython/x64dbgpython/BreakpointDlg.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <Windows.h>
 #include "pluginmain.h"
-#include "CBreakpoints.h"
 
 INT_PTR CALLBACK BreakpointDlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT_PTR CALLBACK ScriptEditorProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
