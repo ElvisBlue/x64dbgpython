fce2e4f32f6b188fa0bd562347d598f4d928e29f
ElvisBlue <elvis200070@gmail.com>
2026-03-12 16:19:08 +0700

    Add wrapper for Misc ParseExpression

--- x64dbgpython/x64dbgpython/scriptapi.cpp
+++ x64dbgpython/x64dbgpython/scriptapi.cpp
@@ -478,6 +478,16 @@ namespace PyWrapper
 		}
 	}
 
+	namespace Misc
+	{
+		duint ParseExpression(std::string expression)
+		{
+			duint value = 0;
+			Script::Misc::ParseExpression(expression.c_str(), &value);
+			return value;
+		}
+	}
+
 	namespace Module
 	{
 		struct pyModuleInfo
@@ -1081,7 +1091,7 @@ PYBIND11_EMBEDDED_MODULE(x64dbg, m)
 	//Implement module Misc
 	py::module mMisc = m.def_submodule("Misc", "x64dbg Misc python script wrapper");
 
-	mMisc.def("ParseExpression", &Misc::ParseExpression);
+	mMisc.def("ParseExpression", &PyWrapper::Misc::ParseExpression);
 	mMisc.def("RemoteGetProcAddress", &Misc::RemoteGetProcAddress);
 	mMisc.def("ResolveLabel", &Misc::ResolveLabel);
 	mMisc.def("Alloc", &Misc::Alloc);
